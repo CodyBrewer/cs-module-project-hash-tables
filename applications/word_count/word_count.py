4be5e02b90d81909f78d891baab12193214114bc
Cody Brewer <codybrewerdev@gmail.com>
2020-10-06 14:27:51 -0600

    word_count completed

--- applications/word_count/word_count.py
+++ applications/word_count/word_count.py
@@ -1,7 +1,21 @@
+import string
+
 def word_count(s):
     # Your code here
-
-
+    s = s.replace("\r", " ")
+    s = s.replace("\n", " ")
+    s = s.replace("\t", " ")
+    words_with_punctuation = s.lower().split(" ")
+    count = {}
+    if len(s) > 0:
+        for i in words_with_punctuation:
+            words = i.strip(string.punctuation)
+            if len(words) > 0:
+                if words in count:
+                    count[words] += 1
+                else:
+                    count[words] = 1
+    return count
 
 if __name__ == "__main__":
     print(word_count(""))
